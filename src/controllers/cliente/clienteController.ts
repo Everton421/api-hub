09d502c08d76e9ed290bc65385f55710196b4970
unknown <evertonjose421@gmail.com>
2025-03-19 08:53:33 -0300

    190320250853

--- src/controllers/cliente/clienteController.ts
+++ src/controllers/cliente/clienteController.ts
@@ -3,67 +3,13 @@ import { Select_clientes } from "../../models/cliente/select";
 import { Insert_clientes } from "../../models/cliente/insert";
 import { Cliente } from "../../models/cliente/interface_cliente";  
 import { Update_clientes } from "../../models/cliente/update";
+import { DateService } from "../../services/dateService";
 
 export class ClienteController{
 
-    formatarDataEhora1(data: string): string | null {
-        const regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
-        if (!regex.test(data)) {
-            return null;
-          }
-         return data;
-         }
+ 
 
-         formatarDataHora(data: any): string {
-            if (!(data instanceof Date)) {
-              try {
-                data = new Date(data); // Tenta converter para Date
-              } catch (error) {
-                console.error("Data inválida:", data, error);
-                return "Data inválida"; // Ou outra string de erro
-              }
-          
-              if (isNaN(data.getTime())) {
-                console.error("Data inválida após tentativa de conversão:", data);
-                return "Data inválida"; // Ou outra string de erro
-              }
-            }
-          
-            const dia = String(data.getDate()).padStart(2, '0');
-            const mes = String(data.getMonth() + 1).padStart(2, '0');
-            const ano = data.getFullYear();
-          
-            const hora = String(data.getHours()).padStart(2, '0'); // Garante dois dígitos
-            const minuto = String(data.getMinutes()).padStart(2, '0'); // Garante dois dígitos
-            const segundos = String(data.getSeconds()).padStart(2, '0'); // Garante dois dígitos
-          
-            return `${ano}-${mes}-${dia} ${hora}:${minuto}:${segundos}`;
-          }
-
-           formatarData(data:any) {
-            const dia = String(data.getDate()).padStart(2, '0');
-            const mes = String(data.getMonth() + 1).padStart(2, '0');
-            const ano = data.getFullYear();
-            return `${ano}-${mes}-${dia}`;
-        }
-
-         obterDataAtual() {
-            const dataAtual = new Date();
-            const dia = String(dataAtual.getDate()).padStart(2, '0');
-            const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-            const ano = dataAtual.getFullYear();
-            return `${ano}-${mes}-${dia}`;
-        }
-           obterDataHoraAtual() {
-            const dataAtual = new Date();
-            const dia = String(dataAtual.getDate()).padStart(2, '0');
-            const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-            const ano = dataAtual.getFullYear();
-            const hora = dataAtual.getHours();
-            const minuto = dataAtual.getMinutes();
-            const segundos = dataAtual.getSeconds();
-            return `${ano}-${mes}-${dia} ${hora}:${minuto}:${segundos}`;
-        }
+       
 
     async buscaGeral( req:Request,res:Response  ){
         let empresa:any   = req.headers.cnpj 
@@ -109,6 +55,7 @@ export class ClienteController{
         let select = new Select_clientes();
         let insert = new Insert_clientes();
 
+        let dateService = new DateService();
 
         if(!req.headers.cnpj ){
             return res.status(200).json({erro:true,msg:"É necessario informar a empresa "});   
@@ -131,15 +78,15 @@ export class ClienteController{
                 if (!postCliente.cidade)          postCliente.cidade = "";
             
                 if (!postCliente.data_cadastro || postCliente.data_cadastro ===  "0000-00-00") {
-                    postCliente.data_cadastro = obj.obterDataAtual();
+                    postCliente.data_cadastro = dateService.obterDataAtual();
                 }else{
-                    postCliente.data_cadastro = obj.formatarData(postCliente.data_cadastro);
+                    postCliente.data_cadastro = dateService.formatarData(postCliente.data_cadastro);
                 }
             
                 if (!postCliente.data_recadastro || postCliente.data_recadastro === "0000-00-00 00:00:00" ){ 
-                        postCliente.data_recadastro = obj.obterDataHoraAtual()
+                        postCliente.data_recadastro = dateService.obterDataHoraAtual()
                     } else{
-                        postCliente.data_recadastro = obj.formatarDataHora(postCliente.data_recadastro ) 
+                        postCliente.data_recadastro = dateService.formatarDataHora(postCliente.data_recadastro ) 
                     };
             
                 if (!postCliente.vendedor)        postCliente.vendedor = 0;
@@ -264,6 +211,7 @@ export class ClienteController{
             let select = new Select_clientes();
             let insert = new Insert_clientes();
             let update = new Update_clientes();
+            let dateService = new DateService();
 
     
             if(!req.headers.cnpj ){
@@ -286,13 +234,13 @@ export class ClienteController{
                     if (!postCliente.cidade)          postCliente.cidade = "";
                     if(!postCliente.ativo)            postCliente.ativo = "S";
                     if (!postCliente.data_cadastro  ) {
-                        postCliente.data_cadastro = obj.obterDataAtual();
+                        postCliente.data_cadastro = dateService.obterDataAtual();
                     } 
                 
                     if (!postCliente.data_recadastro || postCliente.data_recadastro === "0000-00-00 00:00:00" ){ 
-                            postCliente.data_recadastro = obj.obterDataHoraAtual()
+                            postCliente.data_recadastro = dateService.obterDataHoraAtual()
                         } else{
-                                postCliente.data_recadastro = obj.obterDataHoraAtual();
+                                postCliente.data_recadastro = dateService.obterDataHoraAtual();
                         };
                 
                     if (!postCliente.vendedor)        postCliente.vendedor = 0;
