806aaa711c350934ac6c3fd40c917c3f83ada706
unknown <evertonjose421@gmail.com>
2025-08-02 12:02:12 -0300

    '020820251202'

--- src/controllers/distribuicao-locais/distribuicao-controller.ts
+++ src/controllers/distribuicao-locais/distribuicao-controller.ts
@@ -1,9 +1,14 @@
+import { Request, Response } from "express";
 
 export class DistribuicaoController{
 
 
-    async busca(){
-        
+    async update(req:Request, res:Response ){
+
+            /// ****
+            // recceber os dados, buscar o produto + local + setor, se encontrar comparar a data de recadastro
+            // atualizar caso necessario 
+            // inserir caso nao existir  
     }
 
 }
--- src/models/distribuicao_locais_setor/select.ts
+++ src/models/distribuicao_locais_setor/select.ts
@@ -65,4 +65,65 @@ export class SelectDistribuicaoSetor{
             })
 
     }
+
+     async selectByParamUpdate(empresa:string, query:Partial<IDistribuicaoLocaisSetor> ){
+        return new Promise( async (resolve, reject)=>{
+    if (Object.keys(query).length <= 1) {
+                        return reject(new Error("Nenhum campo fornecido para filtrar."));
+                    }
+
+            let sql = ` SELECT *,
+                DATE_FORMAT(data_cadastro, '%Y-%m-%d') AS data_cadastro,
+                DATE_FORMAT(data_recadastro, '%Y-%m-%d %H:%i:%s') AS data_recadastro
+              FROM ${empresa}.locais `;
+
+                let conditions =[]
+                let values =[]
+
+                if(query.produto ){
+                    conditions.push(' produto = ? ');
+                    values.push(`${query.produto}`)
+                }
+                if(query.setor ){
+                    conditions.push(' setor = ? ');
+                    values.push(`${query.setor}`)
+                }
+                if(query.local ){
+                    conditions.push(' local = ? ');
+                    values.push(`${query.local}`)
+                }
+                if(query.unidade_medida ){
+                    conditions.push(' unidade_medida = ? ');
+                    values.push(`${query.unidade_medida}`)
+                }
+                if(query.quantidade ){
+                    conditions.push(' quantidade = ? ');
+                    values.push(`${query.quantidade}`)
+                }
+                if(query.data_cadastro ){
+                    conditions.push(' data_cadastro = ? ');
+                    values.push(`${query.data_cadastro}`)
+                }
+                if(query.data_recadastro ){
+                    conditions.push(' data_recadastro  > ? ');
+                    values.push(`${query.data_recadastro}`)
+                }
+             let finalSql = sql
+                
+             let whereClause = ' WHERE  '
+
+               if( conditions.length > 0 ){
+                    finalSql = sql + conditions.join(' AND ') + whereClause
+               }
+
+               await conn.query( finalSql , values ,(err:any, result :any)=>{
+                            if(err){
+                                reject(err);
+                            }else{
+                                resolve(result);
+                            }
+                        })
+            })
+
+    }
 }
