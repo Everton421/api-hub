2386c2de632a7cede482217a418f6c8126e39db7
everton421 <evertonjose421@gmail.com>
2026-02-13 11:52:06 -0300

    13 de fev 2026 [ test mudança no insert do produto ]

---
+++ src/__test__/test-replace-string.ts
@@ -0,0 +1,6 @@
+import { FormatString } from "../utils/format-string";
+
+const  aux  = new FormatString(); 
+ 
+const str = aux.replaceAspasDuplas('LIMA CHATA P/ ENXADA 8\\" MASTER 41702/308 TRAMONTINA');
+console.log(str);
